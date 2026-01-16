8cf8168934d973f256d21d141e0755dcdbdaa611
Gabriel <gabriel_.brito@hotmail.com>
2026-01-16 13:07:27 -0300

    Feat: Implementação de cadastro de usuario

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,6 +1,68 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { ClipLoader } from "react-spinners"
+import type Usuario from "../../models/Usuario"
+import { cadastraUsuario } from "../../services/Service"
 
 function Cadastro() {
+
+    const navigate = useNavigate()
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const [confirmarSenha, setConfirmarSenha] = useState<string>("")
+
+    const [usuario, setUsuario] = useState<Usuario>({
+        id: 0,
+        nome: '',
+        usuario: '',
+        senha: '',
+        foto: ''
+    })
+
+    useEffect(() => {
+        if (usuario.id !== 0) {
+            retornar()
+        }
+    }, [usuario])
+
+    function retornar() {
+        navigate('/')
+    }
+
+    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+        setUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
+        setConfirmarSenha(e.target.value)
+    }
+
+    async function cadastraNovoUsuario(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if (confirmarSenha === usuario.senha && usuario.senha.length >= 8) {
+            setIsLoading(true)
+
+            try {
+                await cadastraUsuario(`/usuarios/cadastrar`, usuario, setUsuario)
+                alert('Usuario Cadastrado com sucesso!')
+            } catch (error) {
+                alert('Erro ao  cadastrar o usuario!')
+            }
+        } else {
+            alert('Dados do usuario incosistentes! Verifique as informações de cadastro.')
+            setUsuario({ ...usuario, senha: '' })
+            setConfirmarSenha('')
+        }
+
+        setIsLoading(false)
+    }
+
+
     return (
 
         <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen bg-slate-900 text-white font-bold">
@@ -12,7 +74,8 @@ function Cadastro() {
 
             {/* Lado Direito: Formulário */}
             <div className="flex justify-center items-center p-8">
-                <form className="w-full max-w-md flex flex-col gap-6">
+                <form className="w-full max-w-md flex flex-col gap-6"
+                    onSubmit={cadastraNovoUsuario}>
 
                     <h2 className="text-5xl text-teal-500 text-center mb-4">Cadastrar</h2>
 
@@ -25,6 +88,8 @@ function Cadastro() {
                             name="nome"
                             placeholder="Seu nome completo"
                             className="rounded p-3 bg-slate-800 border-2 border-slate-700 placeholder-slate-500 focus:border-teal-400 focus:outline-none transition-all"
+                            value={usuario.nome}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
 
@@ -37,6 +102,8 @@ function Cadastro() {
                             name="usuario"
                             placeholder="Seu e-mail (ex: usuario@email.com)"
                             className="rounded p-3 bg-slate-800 border-2 border-slate-700 placeholder-slate-500 focus:border-teal-400 focus:outline-none transition-all"
+                            value={usuario.usuario}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
 
@@ -49,6 +116,8 @@ function Cadastro() {
                             name="foto"
                             placeholder="Link da sua foto"
                             className="rounded p-3 bg-slate-800 border-2 border-slate-700 placeholder-slate-500 focus:border-teal-400 focus:outline-none transition-all"
+                            value={usuario.foto}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
 
@@ -61,6 +130,8 @@ function Cadastro() {
                             name="senha"
                             placeholder="Senha segura"
                             className="rounded p-3 bg-slate-800 border-2 border-slate-700 placeholder-slate-500 focus:border-teal-400 focus:outline-none transition-all"
+                            value={usuario.senha}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
 
@@ -73,6 +144,8 @@ function Cadastro() {
                             name="confirmarSenha"
                             placeholder="Confirme sua senha"
                             className="rounded p-3 bg-slate-800 border-2 border-slate-700 placeholder-slate-500 focus:border-teal-400 focus:outline-none transition-all"
+                            value={confirmarSenha}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
                         />
                     </div>
 
@@ -81,6 +154,7 @@ function Cadastro() {
                         <button
                             type='button'
                             className="w-1/2 rounded bg-red-500 py-3 font-bold text-white hover:bg-red-600 hover:scale-105 transition-all flex justify-center"
+                            onClick={retornar}
                         >
                             Cancelar
                         </button>
@@ -89,7 +163,13 @@ function Cadastro() {
                             type='submit'
                             className="w-1/2 rounded bg-teal-500 py-3 font-bold text-slate-900 hover:bg-teal-400 hover:scale-105 transition-all flex justify-center"
                         >
-                            Cadastrar
+                            {isLoading ?
+                                <ClipLoader
+                                    color="#ffffff"
+                                    size={24}
+                                /> :
+                                <span>Cadastrar</span>
+                            }
                         </button>
 
                         <div className="text-center mt-2 border-t border-slate-700/50 pt-4">
