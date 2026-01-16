3c0f4f654fa3a0a7f3e11a11f38c3e952140217c
Gabriel <gabriel_.brito@hotmail.com>
2026-01-16 10:55:52 -0300

    Feat: Adicionando models

---
+++ src/models/Postagem.ts
@@ -0,0 +1,11 @@
+import type Tema from "./tema";
+import type Usuario from "./usuario";
+
+export default interface Postagem {
+    id: number;
+    titulo: string;
+    texto: string;
+    data: string;
+    tema: Tema | null;
+    usuario: Usuario | null;
+}
