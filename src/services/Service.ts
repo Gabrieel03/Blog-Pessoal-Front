c553befbe09b6c0827b96bcc706e221938aae48a
Gabriel <gabriel_.brito@hotmail.com>
2026-01-23 13:56:46 -0300

    Deplay do projeto BlogPessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: 'https://projeto-blogpessoal-3vsc.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastraUsuario = async (url: string, dados: Object, setDados: Function) => {
